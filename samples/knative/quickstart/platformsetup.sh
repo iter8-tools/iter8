a2cbd68c9fb2bc61d166f3673c3720488258834c
Michael Kalantar <kalantar@us.ibm.com>
2021-04-14 12:48:02 -0400

    update doc in response to changes to install

--- samples/knative/quickstart/platformsetup.sh
+++ samples/knative/quickstart/platformsetup.sh
@@ -117,7 +117,7 @@ fi
 echo "Installing Iter8 with Knative support"
 kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/core/build.yaml
 
-# Step 6: Install Iter8's Prometheus add-on
+# Step 6: Install sample metrics for Knative and Iter8's Prometheus add-on
 echo "Installing Iter8's Prometheus add-on"
 kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/prometheus-add-on/prometheus-operator/build.yaml
 
