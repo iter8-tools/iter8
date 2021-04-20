79aa5f8d8d52f0d315a4b7ec6dcffa9f098c9a56
Michael Kalantar <kalantar@us.ibm.com>
2021-04-20 13:11:08 -0400

    gain insight into failure

--- samples/knative/canaryprogressive/check.sh
+++ samples/knative/canaryprogressive/check.sh
@@ -2,23 +2,39 @@
 
 set -e
 
+EXPERIMENT=canary-progressive
+
+# dump logs from iter8 pods
+dump() {
+    # dump handler logs
+    for pod in $(kubectl -n iter8-system get po --selector=iter8/experimentName=${EXPERIMENT} -o jsonpath='{.items[*].metadata.name}'); do 
+        kubectl -n iter8-system logs $pod
+    done
+
+    # dump controller logs
+    kubectl -n iter8-system logs $(kubectl -n iter8-system get po --selector=control-plane=controller-manager -o jsonpath='{.items[0].metadata.name}')
+    
+    # dump analytics logs
+    kubectl -n iter8-system logs $(kubectl -n iter8-system get po --selector=app=iter8-analytics -o jsonpath='{.items[0].metadata.name}')
+}
+
 # Ensure ITER8 environment variable is set
 if [[ -z ${ITER8} ]]; then
     echo "ITER8 environment variable needs to be set to the root folder of Iter8"
-    exit 1
+    dump; exit 1
 else
     echo "ITER8 is set to " $ITER8
 fi
 
 # Check if experiment has completed
 completed="Completed"
-stage=$(kubectl get experiment canary-progressive -o json | jq -r .status.stage)
+stage=$(kubectl get experiment ${EXPERIMENT} -o json | jq -r .status.stage)
 if [[ $stage = $completed ]]; then
     echo "Experiment has Completed"
 else
     echo "Experiment must be $completed;" 
     echo "Experiment is $stage;"
-    exit 1
+    dump; exit 1
 fi
 
 # Check if no packets have been lost by Fortio
@@ -33,17 +49,17 @@ else
     echo "Packets were lost"
     echo "total requests:" $REQUESTSTOTAL
     echo "200 requests:" $REQUESTS200
-    exit 1
+    dump; exit 1
 fi
 
 # Check if versionRecommendedForPromotion is candidate
 candidate="candidate"
-vrfp=$(kubectl get experiment canary-progressive -o json | jq -r .status.versionRecommendedForPromotion)
+vrfp=$(kubectl get experiment  ${EXPERIMENT} -o json | jq -r .status.versionRecommendedForPromotion)
 if [[ $vrfp = $candidate ]]; then
     echo "versionRecommendedForPromotion is $vrfp"
 else
     echo "versionRecommendedForPromotion must be candidate; is" $vrfp
-    exit 1
+    dump; exit 1
 fi
 
 # Check if latest revision is true
@@ -53,6 +69,7 @@ if [[ $lrStatus = $latestRevision ]]; then
     echo "latestRevision is true"
 else
     echo "latestRevision must be true; is" $lrStatus
+    dump; exit 1
     exit 1
 fi
 
@@ -63,7 +80,7 @@ if [[ $actualPercent -eq $percent ]]; then
     echo "percent is 100"
 else
     echo "percent must be 100; is" $percent
-    exit 1
+    dump; exit 1
 fi
 
 set +e
