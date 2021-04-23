3c2feda2d9c321de1b2ca928f78ac6afd2110982
Srinivasan Parthasarathy <5309125+sriumcp@users.noreply.github.com>
2021-04-23 11:27:08 -0400

    check.sh for mirroring

--- samples/knative/mirroring/check.sh
+++ samples/knative/mirroring/check.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+EXPERIMENT=mirroring
+
+# dump logs from iter8 pods
+dump() {
+    # dump handler logs
+    for pod in $(kubectl -n iter8-system get po --selector=iter8/experimentName=${EXPERIMENT} -o jsonpath='{.items[*].metadata.name}'); do 
+        kubectl -n iter8-system logs $pod
+    done
+
+    # dump controller logs
+    kubectl -n iter8-system logs $(kubectl -n iter8-system get po --selector=control-plane=controller-manager -o jsonpath='{.items[0].metadata.name}')
+    
+    # dump analytics logs
+    kubectl -n iter8-system logs $(kubectl -n iter8-system get po --selector=app=iter8-analytics -o jsonpath='{.items[0].metadata.name}')
+}
+
 # Ensure ITER8 environment variable is set
 if [[ -z ${ITER8} ]]; then
     echo "ITER8 environment variable needs to be set to the root folder of Iter8"
@@ -22,6 +38,14 @@ else
     exit 1
 fi
 
-# check for winner?
+# Check if winner is baseline
+expectedwinner="current"
+winner=$(kubectl get experiment ${EXPERIMENT} -o json | jq -r .status.analysis.winnerAssessment.data.winner)
+if [[ $winner = $expectedwinner ]]; then
+    echo "winner is $winner"
+else
+    echo "winner must be current; is" $winner
+    dump; exit 1
+fi
 
 set +e
