ba05fbc5b452286d13549bcf4aa1afd3e3dbbfb1
Michael Kalantar <kalantar@us.ibm.com>
2021-05-07 10:46:08 -0400

    istio conformance tutorial

---
+++ samples/istio/conformance/e2etest.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e -x
+
+export EXPERIMENT=conformance-exp
+
+# method to report failure
+reportFailure () {
+    status=$?
+    if (( $status != 0 )); then
+        echo -e "\033[0;31mFAILED:\033[0m $0"
+    fi
+    exit $status
+}
+trap "reportFailure" EXIT
+
+# create cluster
+kind create cluster
+kubectl cluster-info --context kind-kind
+
+# platform setup
+echo "Setting up platform"
+$ITER8/samples/istio/quickstart/platformsetup.sh
+
+echo "Create bookinfo app with two productpage versions"
+kubectl apply -n bookinfo-iter8 -f $ITER8/samples/istio/conformance/bookinfo-app.yaml
+
+echo "Generate requests"
+kubectl -n bookinfo-iter8 wait --for=condition=Ready pods --all --timeout=600s
+URL_VALUE="http://$(kubectl -n istio-system get svc istio-ingressgateway -o jsonpath='{.spec.clusterIP}'):80/productpage"
+sed "s+URL_VALUE+${URL_VALUE}+g" $ITER8/samples/istio/conformance/fortio.yaml | sed "s/6000s/120s/g" | kubectl apply -f -
+pod_name=$(kubectl get pods --selector=job-name=fortio -o jsonpath='{.items[*].metadata.name}')
+kubectl wait --for=condition=Ready pods/"$pod_name" --timeout=240s
+
+echo "Defining metrics"
+kubectl apply -f $ITER8/samples/istio/quickstart/metrics.yaml
+
+echo "Creating A/B experiment"
+kubectl apply -f $ITER8/samples/istio/conformance/experiment.yaml
+
+# Wait
+kubectl wait experiment $EXPERIMENT --for=condition=Completed --timeout=300s
+
+# Log final experiment
+kubectl get experiment $EXPERIMENT -o yaml
+
+# Check
+source $ITER8/samples/istio/conformance/check.sh
+
+# Cleanup .. not needed since cluster is getting deleted
+# included to test manual instructions
+# continue even if errors
+kubectl delete -f $ITER8/samples/istio/conformance/fortio.yaml
+kubectl delete -f $ITER8/samples/istio/conformance/experiment.yaml
+kubectl delete namespace bookinfo-iter8
+
+# delete cluster
+kind delete cluster
+
+set +e
+
+echo -e "\033[0;32mSUCCESS:\033[0m $0"
