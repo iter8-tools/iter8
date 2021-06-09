ad891cd747ca0df7a72b0b1a07ae36abea8d2f86
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-06-09 09:39:03 -0400

    all systems go
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- install/docker/iter8.sh
+++ install/docker/iter8.sh
@@ -7,9 +7,15 @@ do
   sleep 1
 done
 
+echo "Docker in Docker is running..."
+
 kind create cluster --wait 5m
 kubectl cluster-info --context kind-kind
 
+echo "Installing Iter8..."
 export TAG=master
 kustomize build github.com/iter8-tools/iter8/install/core/?ref=${TAG} | kubectl apply -f -
 kubectl wait --for=condition=Ready pods --all -n iter8-system
+
+echo "Iter8 in Docker is running..."
+echo "All systems go."
