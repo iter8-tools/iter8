bc544cbf6c8bfd517cd763addbfabe8c534c16d7
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-06-14 08:30:55 -0400

    wait until completion of experiment
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- samples/e2e-tests/race-condition/check.sh
+++ samples/e2e-tests/race-condition/check.sh
@@ -26,17 +26,6 @@ else
     echo "ITER8 is set to " $ITER8
 fi
 
-# Check if experiment has completed
-completed="Completed"
-stage=$(kubectl get experiment ${EXPERIMENT} -o json | jq -r .status.stage)
-if [[ $stage = $completed ]]; then
-    echo "Experiment has Completed"
-else
-    echo "Experiment must be $completed;" 
-    echo "Experiment is $stage;"
-    exit 1
-fi
-
 # Check if winner is baseline
 expectedwinner="sample-app-v1"
 winner=$(kubectl get experiment ${EXPERIMENT} -o json | jq -r .status.analysis.winnerAssessment.data.winner)
