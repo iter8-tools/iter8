a6cfe3eacf5b7caf65e0a8f9850ab09910c89522
Hai Huang <huang195@gmail.com>
2021-06-24 09:38:17 -0400

    do not change deployment label
    Signed-off-by: Hai Huang <huang195@gmail.com>

--- samples/istio/gitops/runCI.sh
+++ samples/istio/gitops/runCI.sh
@@ -10,7 +10,5 @@ sed "s|name: gitops-exp|name: gitops-exp-$RANDOM|" templates/experiment.yaml > .
 # use a random color for a new experiment candidate
 declare -a colors=("red" "orange" "blue" "green" "yellow" "violet" "brown")
 color=`expr $RANDOM % ${#colors[@]}`
-version=`git rev-parse --short HEAD`
-sed "s|value: COLOR|value: \"${colors[$color]}\"|" templates/productpage-candidate.yaml |\
-sed "s|version: v.*|version: v$version|" > ./productpage-candidate.yaml
+sed "s|value: COLOR|value: \"${colors[$color]}\"|" templates/productpage-candidate.yaml > ./productpage-candidate.yaml
 
