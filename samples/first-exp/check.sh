77377a0e6f83aeef1ee94f8bf6876391b3e52eba
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-07-15 09:55:22 -0400

    common iter8-setup.sh
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

---
+++ samples/first-exp/check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+# Ensure ITER8 environment variable is set
+if [[ -z ${ITER8} ]]; then
+    echo "ITER8 environment variable needs to be set to the root folder of Iter8"
+    exit 1
+else
+    echo "ITER8 is set to " $ITER8
+fi
+
+# Check if experiment has completed
+completed="Completed"
+stage=$(kubectl get experiment ${EXPERIMENT} -o json | jq -r .status.stage)
+if [[ $stage = $completed ]]; then
+    echo "Experiment has Completed"
+else
+    echo "Experiment must be $completed;" 
+    echo "Experiment is $stage;"
+    exit 1
+fi
+
+# Check if winner is baseline
+expectedwinner="baseline"
+winner=$(kubectl get experiment ${EXPERIMENT} -o json | jq -r .status.analysis.winnerAssessment.data.winner)
+if [[ $winner = $expectedwinner ]]; then
+    echo "winner is $winner"
+else
+    echo "winner must be $expectedwinner; is" $winner
+    dump; exit 1
+fi
+
+set +e
