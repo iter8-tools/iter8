dabbf29dc84aeb4f7c5b52b0dde5366bf1300790
Alan Cha <Alan.cha1@ibm.com>
2021-08-20 10:38:03 -0400

    Change install version GitHub Workflow
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

---
+++ .github/workflows/change-install-version.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Used by ind.yaml
+# Change the install version in mkdocs.yaml
+
+# Usage: python3 change-install-version.py <version number>
+
+import sys
+from ruamel.yaml import YAML
+
+if len(sys.argv) < 2:
+  raise ValueError('Please provide an install version as a value.')
+
+new_install_version=sys.argv[1]
+mkdocs_file_path='../../mkdocs/mkdocs.yml'
+
+with open(mkdocs_file_path, 'r') as fp:
+  # Read YAML
+  yaml=YAML()
+  data=yaml.load(fp)
+
+  # Replace install version
+  data['extra']['iter8']['install_version']=new_install_version
+
+with open(mkdocs_file_path, 'w') as fp:
+  # Rewrite YAML
+  yaml.dump(data, fp)
