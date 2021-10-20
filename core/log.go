c67a4b517c73a4b0c85e4e8c331147f8bdcb3061
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-10-20 09:04:19 -0400

    stack tracing
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- core/log.go
+++ core/log.go
@@ -7,13 +7,38 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var Logger *logrus.Logger
+// Iter8Logger inherits all methods from logrus logger
+type Iter8Logger struct {
+	*logrus.Logger
+}
 
+// StackTrace is the trace from external components like a shell scripts run by an Iter8 task
 type StackTrace struct {
 	Trace string
 }
 
-func (st StackTrace) String() string {
+// Logger to be used in all of Iter8
+var Logger *Iter8Logger
+
+// Initialize logger
+func init() {
+	Logger = &Iter8Logger{logrus.New()}
+	Logger.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   true,
+		DisableQuote:    true,
+		DisableSorting:  true,
+	})
+}
+
+// WithStackTrace yields a log entry with a formatted stack trace field embedded in it
+func (l *Iter8Logger) WithStackTrace(t string) *logrus.Entry {
+	return l.WithField("stack-trace", &StackTrace{
+		Trace: t,
+	})
+}
+
+func (st *StackTrace) String() string {
 	out := "stack trace below ... \n"
 	scanner := bufio.NewScanner(strings.NewReader(st.Trace))
 	for scanner.Scan() {
@@ -21,14 +46,4 @@ func (st StackTrace) String() string {
 	}
 	out = strings.TrimSuffix(out, "\n")
 	return out
-}
-
-func init() {
-	Logger = logrus.New()
-	Logger.SetFormatter(&logrus.TextFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
-		FullTimestamp:   true,
-		DisableQuote:    true,
-		DisableSorting:  true,
-	})
 }
