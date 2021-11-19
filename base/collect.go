cf09f584f395430a66ba6ce71fde6d7596a57b24
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-11-19 16:10:22 -0500

    num threads
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- base/collect.go
+++ base/collect.go
@@ -148,7 +148,7 @@ func (t *collectTask) getFortioOptions(j int) (*fhttp.HTTPRunnerOptions, error) 
 		RunnerOptions: periodic.RunnerOptions{
 			RunType:     "Iter8 load test",
 			QPS:         float64(*t.With.QPS),
-			NumThreads:  4,
+			NumThreads:  *t.With.Connections,
 			Percentiles: t.With.Percentiles,
 			Out:         io.Discard,
 		},
