8d4c86c8754fe05ecd2e8d9a931692240d9bf15f
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-11-19 08:52:55 -0500

    starting to refactor gen
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- cmd/gen.go
+++ cmd/gen.go
@@ -20,8 +20,8 @@ const (
 )
 
 var (
-	// values are user specified values used during gen
-	values []string
+	// Output format
+	outputFormat string = "text"
 )
 
 type ExperimentWithValues struct {
@@ -57,7 +57,37 @@ var GenCmd = &cobra.Command{
 	# generate formatted text output
 	iter8 gen
 `,
-	RunE: func(cmd *cobra.Command, args []string) error {
+	Run: func(cmd *cobra.Command, args []string) {
+		var tmpl *template.Template
+		var err error
+
+		switch strings.ToLower(outputFormat) {
+		case "text":
+			tmpl = builtInTemplates["text"]
+
+		case "custom":
+			// read in the template file
+			tplBytes, err := ioutil.ReadFile(templateFilePath)
+			if err != nil {
+				log.Logger.WithStackTrace(err.Error()).Error("unable to read template file")
+				os.Exit(1)
+			}
+
+			// ensure it is a valid template
+			tmpl, err = template.New("tpl").Funcs(template.FuncMap{
+				"toYAML": toYAML,
+			}).Option("missingkey=error").Funcs(sprig.TxtFuncMap()).Parse(string(tplBytes))
+			if err != nil {
+				log.Logger.WithStackTrace(err.Error()).Error("unable to parse template file")
+				os.Exit(1)
+			}
+
+		default:
+			log.Logger.Error("invalid output format; valid formats are: text | custom")
+			os.Exit(1)
+		}
+
+		// build experiment
 		log.Logger.Trace("build started")
 		// build experiment
 		// replace FileExpIO with ClusterExpIO to build from cluster
