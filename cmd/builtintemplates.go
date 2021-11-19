403509a41d5b101afec76813ff871be1fb54f3be
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-11-19 08:52:55 -0500

    starting to refactor gen
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- cmd/builtintemplates.go
+++ cmd/builtintemplates.go
@@ -1,19 +1,32 @@
 package cmd
 
 import (
-	ht "html/template"
+	"os"
 	"text/template"
+
+	"github.com/Masterminds/sprig"
+	"github.com/iter8-tools/iter8/base/log"
 )
 
-// built-in templates that can be used for reporting experiment results
-var builtInTemplates = make(map[string]executable)
+// built-in template that can be used for reporting experiment results
+var builtInTemplates = make(map[string]*template.Template)
 
-// RegisterTextTemplate registers a text template
-func RegisterTextTemplate(name string, tpl *template.Template) {
+// Register template adds a template to builtInTemplates
+func RegisterTemplate(name string, tpl *template.Template) {
 	builtInTemplates[name] = tpl
 }
 
-// RegisterHTMLTemplate registers an HTML template
-func RegisterHTMLTemplate(name string, tpl *ht.Template) {
-	builtInTemplates[name] = tpl
+func init() {
+
+	// create text template
+	tmpl, err := template.New("text").Funcs(template.FuncMap{
+		"formatText": formatText,
+	}).Option("missingkey=error").Funcs(sprig.TxtFuncMap()).Parse("{{ formatText . }}")
+	if err != nil {
+		log.Logger.WithStackTrace(err.Error()).Error("unable to parse text template")
+		os.Exit(1)
+	}
+
+	// register text template
+	RegisterTemplate("text", tmpl)
 }
--- cmd/gen.go
+++ cmd/gen.go
@@ -21,8 +21,8 @@ const (
 )
 
 var (
-	// values are user specified values used during gen
-	values []string
+	// Output format
+	outputFormat string = "text"
 )
 
 type ExperimentWithValues struct {
@@ -59,6 +59,36 @@ var GenCmd = &cobra.Command{
 	iter8 gen
 `,
 	Run: func(cmd *cobra.Command, args []string) {
+		var tmpl *template.Template
+		var err error
+
+		switch strings.ToLower(outputFormat) {
+		case "text":
+			tmpl = builtInTemplates["text"]
+
+		case "custom":
+			// read in the template file
+			tplBytes, err := ioutil.ReadFile(templateFilePath)
+			if err != nil {
+				log.Logger.WithStackTrace(err.Error()).Error("unable to read template file")
+				os.Exit(1)
+			}
+
+			// ensure it is a valid template
+			tmpl, err = template.New("tpl").Funcs(template.FuncMap{
+				"toYAML": toYAML,
+			}).Option("missingkey=error").Funcs(sprig.TxtFuncMap()).Parse(string(tplBytes))
+			if err != nil {
+				log.Logger.WithStackTrace(err.Error()).Error("unable to parse template file")
+				os.Exit(1)
+			}
+
+		default:
+			log.Logger.Error("invalid output format; valid formats are: text | custom")
+			os.Exit(1)
+		}
+
+		// build experiment
 		log.Logger.Trace("build started")
 		// build experiment
 		// replace FileExpIO with ClusterExpIO to build from cluster
