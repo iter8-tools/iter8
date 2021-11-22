1e9313056349c5e8e49ccab26603db2dafe809be
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-11-22 13:59:08 -0500

    experiments are being validated
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- cmd/experiment.go
+++ cmd/experiment.go
@@ -11,7 +11,7 @@ import (
 )
 
 type Experiment struct {
-	tasks []base.Task `validate:"required"`
+	tasks []base.Task
 	*base.Experiment
 }
 
@@ -75,13 +75,6 @@ func Build(withResult bool, expio ExpIO) (*Experiment, error) {
 				return nil, errors.New("unknown task: " + *t.Task)
 			}
 
-			if err != nil {
-				return nil, err
-			}
-
-			validate := validator.New()
-			// returns nil or ValidationErrors ( []FieldError )
-			err := validate.Struct(e)
 			if err != nil {
 				return nil, err
 			}
