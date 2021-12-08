5a0fd99cd82c1c830da232fbe454b6278b425341
Srinivasan Parthasarathy <spartha@us.ibm.com>
2021-12-08 17:02:49 -0500

    test run assess
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- base/collect_test.go
+++ base/collect_test.go
@@ -66,49 +66,4 @@ func TestMakeCollect(t *testing.T) {
 	task, err = MakeCollect(ts)
 	assert.NoError(t, err)
 	assert.NotNil(t, task)
-
 }
-
-// // Test a runnable assert condition here
-// func TestRunAssess(t *testing.T) {
-// 	// simple assess without any SLOs
-// 	// should succeed
-// 	ts := &TaskSpec{
-// 		taskMeta: taskMeta{
-// 			Task: stringPointer(AssessTaskName),
-// 		},
-// 	}
-// 	task, _ := MakeAssess(ts)
-// 	exp := &Experiment{
-// 		Tasks: []TaskSpec{},
-// 	}
-// 	exp.InitResults()
-// 	task.Run(exp)
-
-// 	// assess with an SLO
-// 	// should succeed
-// 	ts = &TaskSpec{
-// 		taskMeta: taskMeta{Task: stringPointer(AssessTaskName)},
-// 		With: map[string]interface{}{
-// 			"SLOs": []SLO{{
-// 				Metric:     "m",
-// 				UpperLimit: float64Pointer(20.0),
-// 			}},
-// 		},
-// 	}
-// 	task, _ = MakeAssess(ts)
-// 	exp = &Experiment{
-// 		Tasks:  []TaskSpec{},
-// 		Result: &ExperimentResult{},
-// 	}
-// 	exp.InitResults()
-// 	exp.Result.InitInsights(1, []InsightType{InsightTypeMetrics})
-// 	err := task.Run(exp)
-// 	assert.NoError(t, err)
-
-// 	// assess with an experiment where num versions is 1
-// 	exp.Result.Insights.NumVersions = 1
-// 	err = task.Run(exp)
-// 	assert.NoError(t, err)
-
-// }
