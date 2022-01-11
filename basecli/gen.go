09290d13351ea0be71497998e62473ae1746eb54
Srinivasan Parthasarathy <spartha@us.ibm.com>
2022-01-11 11:19:24 -0500

    fixing run
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- basecli/gen.go
+++ basecli/gen.go
@@ -2,6 +2,7 @@ package basecli
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"helm.sh/helm/v3/pkg/cli/values"
 )
 
@@ -17,12 +18,15 @@ var genCmd = &cobra.Command{
 	Render templates with values`,
 }
 
-func init() {
-	RootCmd.AddCommand(genCmd)
-	f := genCmd.PersistentFlags()
-	// See: https://github.com/helm/helm/blob/663a896f4a815053445eec4153677ddc24a0a361/cmd/helm/flags.go#L42 which is the source of these persistent flags
+func addGenOptions(f *pflag.FlagSet) {
+	// See: https://github.com/helm/helm/blob/663a896f4a815053445eec4153677ddc24a0a361/cmd/helm/flags.go#L42 which is the source of these flags
 	f.StringSliceVarP(&GenOptions.ValueFiles, "values", "f", []string{}, "specify values in a YAML file or a URL (can specify multiple)")
 	f.StringArrayVar(&GenOptions.Values, "set", []string{}, "set values on the command line (can specify multiple or separate values with commas: key1=val1,key2=val2)")
 	f.StringArrayVar(&GenOptions.StringValues, "set-string", []string{}, "set STRING values on the command line (can specify multiple or separate values with commas: key1=val1,key2=val2)")
 	f.StringArrayVar(&GenOptions.FileValues, "set-file", []string{}, "set values from respective files specified via the command line (can specify multiple or separate values with commas: key1=path1,key2=path2)")
+}
+
+func init() {
+	RootCmd.AddCommand(genCmd)
+	addGenOptions(genCmd.PersistentFlags())
 }
