82a1eb9c11a5f50667cbd7a1af23a25362b58de0
Srinivasan Parthasarathy <spartha@us.ibm.com>
2022-01-25 00:13:14 -0500

    fixing hub location for now
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- basecli/mock_qs_test.go
+++ basecli/mock_qs_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMockQuickStart(t *testing.T) {
+func TestMockQuickStartWithoutSLOs(t *testing.T) {
 	// get into the experiment chart folder
 	os.Chdir(base.CompletePath("../", "hub/load-test"))
 
@@ -37,11 +37,23 @@ func TestMockQuickStart(t *testing.T) {
 	}
 	err = assertCmd.RunE(nil, nil)
 	assert.NoError(t, err)
+}
+
+func TestMockQuickStartWithSLOs(t *testing.T) {
+	// get into the experiment chart folder
+	os.Chdir(base.CompletePath("../", "hub/load-test"))
+
+	// mock the http endpoint
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	// Exact URL match
+	httpmock.RegisterResponder("GET", "https://example.com",
+		httpmock.NewStringResponder(200, `all good`))
 
 	// with SLOs next
 	GenOptions.Values = append(GenOptions.Values, "url=https://example.com", "SLOs.error-rate=0", "SLOs.mean-latency=100")
 	GenOptions.ValueFiles = append(GenOptions.ValueFiles, base.CompletePath("../", "testdata/percentileandslos/values.yaml"))
-	err = runCmd.RunE(nil, nil)
+	err := runCmd.RunE(nil, nil)
 	assert.NoError(t, err)
 
 	// assert
@@ -51,11 +63,23 @@ func TestMockQuickStart(t *testing.T) {
 	}
 	err = assertCmd.RunE(nil, nil)
 	assert.NoError(t, err)
+}
+
+func TestMockQuickStartWithSLOsAndPercentiles(t *testing.T) {
+	// get into the experiment chart folder
+	os.Chdir(base.CompletePath("../", "hub/load-test"))
+
+	// mock the http endpoint
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+	// Exact URL match
+	httpmock.RegisterResponder("GET", "https://example.com",
+		httpmock.NewStringResponder(200, `all good`))
 
 	// with SLOs and percentiles also
 	GenOptions.Values = append(GenOptions.Values, "url=https://example.com", "SLOs.error-rate=0", "SLOs.mean-latency=100", "SLOs.p50=100")
 	GenOptions.ValueFiles = append(GenOptions.ValueFiles, base.CompletePath("../", "testdata/percentileandslos/values.yaml"))
-	err = runCmd.RunE(nil, nil)
+	err := runCmd.RunE(nil, nil)
 	assert.NoError(t, err)
 
 	// assert
@@ -87,7 +111,8 @@ func TestDryRun(t *testing.T) {
 	defer os.RemoveAll(dir)
 
 	os.Chdir(dir)
-	os.Setenv("ITER8HUB", "github.com/iter8-tools/iter8.git?ref=master//hub/")
+	// ToDo: Fix this location
+	os.Setenv("ITER8HUB", "github.com/sriumcp/iter8.git?ref=grpc//hub/")
 	hubFolder = "load-test"
 	// hub
 	err := hubCmd.RunE(nil, nil)
