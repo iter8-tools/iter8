a59495888a3ab54bd63c0b8e21e7d09b81940241
Michael Kalantar <kalantar@us.ibm.com>
2022-01-25 11:15:40 -0500

    simplify inputs
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- load-test/entrypoint.sh
+++ load-test/entrypoint.sh
@@ -19,65 +19,10 @@ echo "Fetch load-test experiment"
 $ITER8 hub -e load-test
 cd load-test
 
+echo "Identify values file"
 OPTIONS=""
-echo "Modify experiment using inputs"
-if [[ ! -z "${INPUT_URL}" ]]; then
-  OPTIONS="$OPTIONS --set url=${INPUT_URL}"
-fi
-if [[ ! -z "${INPUT_NUMQUERIES}" ]]; then
-  OPTIONS="$OPTIONS --set numQueries=${INPUT_NUMQUERIES}"
-fi
-if [[ ! -z "${INPUT_DURATION}" ]]; then
-  OPTIONS="$OPTIONS --set duration=${INPUT_DURATION}"
-fi
-if [[ ! -z "${INPUT_QPS}" ]]; then
-  OPTIONS="$OPTIONS --set qps=${INPUT_QPS}"
-fi
-if [[ ! -z "${INPUT_CONNECTIONS}" ]]; then
-  OPTIONS="$OPTIONS --set connections=${INPUT_CONNECTIONS}"
-fi
-if [[ ! -z "${INPUT_PAYLOADSTR}" ]]; then
-  OPTIONS="$OPTIONS --set payloadStr=\"${INPUT_PAYLOADSTR}\""
-fi
-if [[ ! -z "${INPUT_PAYLOADURL}" ]]; then
-  OPTIONS="$OPTIONS --set payloadUrl=\"${INPUT_PAYLOADURL}\""
-fi
-if [[ ! -z "${INPUT_CONTENTTYPE}" ]]; then
-  OPTIONS="$OPTIONS --set contentType=\"${INPUT_CONTENTTYPE}\""
-fi
-if [[ ! -z "${INPUT_ERROR_RATE}" ]]; then
-  OPTIONS="$OPTIONS --set SLOs.error-rate=${INPUT_ERROR_RATE}"
-fi
-if [[ ! -z "${INPUT_MEAN_LATENCY}" ]]; then
-  OPTIONS="$OPTIONS --set SLOs.mean-latency=${INPUT_MEAN_LATENCY}"
-fi
-if [[ ! -z "${INPUT_P25}" ]]; then
-  OPTIONS="$OPTIONS --set SLOs.p25=${INPUT_P25}"
-fi
-if [[ ! -z "${INPUT_P50}" ]]; then
-  OPTIONS="$OPTIONS --set SLOs.p50=${INPUT_P50}"
-fi
-if [[ ! -z "${INPUT_P75}" ]]; then
-  OPTIONS="$OPTIONS --set SLOs.p75=${INPUT_P75}"
-fi
-if [[ ! -z "${INPUT_P90}" ]]; then
-  OPTIONS="$OPTIONS --set SLOs.p90=${INPUT_P90}"
-fi
-if [[ ! -z "${INPUT_P95}" ]]; then
-  OPTIONS="$OPTIONS --set SLOs.p95=${INPUT_P95}"
-fi
-if [[ ! -z "${INPUT_P97_5}" ]]; then
-  OPTIONS="$OPTIONS --set SLOs.p97\.5=${INPUT_P97_5}"
-fi
-if [[ ! -z "${INPUT_P99}" ]]; then
-  OPTIONS="$OPTIONS --set SLOs.p99=${INPUT_P99}"
-fi
-if [[ ! -z "${INPUT_P99_9}" ]]; then
-  OPTIONS="$OPTIONS --set SLOs.p99\.9=${INPUT_P99_9}"
-fi
-
-if [[ ! -z "${INPUT_VALUES}" ]]; then
-  OPTIONS="$OPTIONS -f ${INPUT_VALUES}"
+if [[ ! -z "${INPUT_VALUESFILE}" ]]; then
+  OPTIONS="$OPTIONS -f ${INPUT_VALUESFILE}"
 fi
 
 # set LOG_LEVEL for iter8 commands
