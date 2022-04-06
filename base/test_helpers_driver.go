948204173a5788341d31d099e9b27d30f2c3d7e5
Michael Kalantar <kalantar@us.ibm.com>
2022-04-06 18:29:24 -0400

    refactor
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- base/test_helpers_driver.go
+++ base/test_helpers_driver.go
@@ -8,33 +8,13 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	dynamicfake "k8s.io/client-go/dynamic/fake"
 	"k8s.io/client-go/kubernetes/fake"
-	ktesting "k8s.io/client-go/testing"
 )
 
 // initKubeFake initialize the Kube clientset with a fake
 func (kd *KubeDriver) initKubeFake(objects ...runtime.Object) {
-	// secretDataReactor sets the secret.Data field based on the values from secret.StringData
-	// Credit: this function is adapted from https://github.com/creydr/go-k8s-utils
-	var secretDataReactor = func(action ktesting.Action) (bool, runtime.Object, error) {
-		secret, _ := action.(ktesting.CreateAction).GetObject().(*corev1.Secret)
-
-		if secret.Data == nil {
-			secret.Data = make(map[string][]byte)
-		}
-
-		for k, v := range secret.StringData {
-			secret.Data[k] = []byte(v)
-		}
-
-		return false, nil, nil
-	}
-
 	fc := fake.NewSimpleClientset(objects...)
-	fc.PrependReactor("create", "secrets", secretDataReactor)
-	fc.PrependReactor("update", "secrets", secretDataReactor)
 	kd.Clientset = fc
-	kd.DynamicClient = dynamicfake.NewSimpleDynamicClient(runtime.NewScheme()) //, newUnstructured("v1", "Pod", "default", "name-foo"))
-	// dc kd.GetObjectFunc = GetFakeObject
+	kd.DynamicClient = dynamicfake.NewSimpleDynamicClient(runtime.NewScheme())
 	kd.Mapping = &FakeKubernetesObjectMapping{}
 }
 
