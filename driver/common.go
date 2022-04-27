a0532fe8e600a2a96044a4b5c3e57216fa80010c
Srinivasan Parthasarathy <spartha@us.ibm.com>
2022-04-26 20:38:32 -0400

    k8s experimentation ... again (#1236)
    * fixing k8s
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * new condition is Available instead of available
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * disable jobs if needed
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * fix kind version
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * fixing tests
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * deploy and available
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * disabling pr tests for now
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * reenabling PR tests
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * k assert instead of assert
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * iter8lib values
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * more fixes for k8s
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * id instead of driver
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * fixing k8s further
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * simplifying tests
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * fix k8s further
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * fix assets test
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * reverting back tests
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * site examples
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * fix init kube
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- driver/common.go
+++ driver/common.go
@@ -9,11 +9,9 @@ import (
 const (
 	// ExperimentSpecPath is the name of the experiment spec file
 	ExperimentSpecPath = "experiment.yaml"
-	// ExperimentSpecPath is the name of the experiment result file
+	// ExperimentResultPath is the name of the experiment result file
 	ExperimentResultPath = "result.yaml"
-	// ExperimentSpecPath is the name of the official Iter8 experiment chart repo
-	DefaultIter8RepoURL = "https://iter8-tools.github.io/hub"
-	// ExperimentSpecPath is the name of the default experiment chart
+	// DefaultExperimentGroup is the name of the default experiment chart
 	DefaultExperimentGroup = "default"
 )
 
