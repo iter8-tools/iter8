122cc4d9095c54888d56f37dd90f2c802afe9160
Michael Kalantar <kalantar@us.ibm.com>
2022-06-29 14:13:00 -0400

    remove consistent package
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- abn/watcher/lookup.go
+++ abn/watcher/lookup.go
@@ -3,11 +3,39 @@ package watcher
 // lookup.go -(internal) implementation of gRPC Lookup method
 
 import (
+	"bytes"
 	"errors"
+	"hash/crc32"
 
 	"github.com/iter8-tools/iter8/abn/util"
 	"github.com/iter8-tools/iter8/base/log"
 )
+
+func rendezvousGet(name string, key string) string {
+	var maxScore uint32
+	var maxNode []byte
+
+	keyBytes := []byte(key)
+	for t, v := range apps[name].tracks {
+		vBytes := []byte(v)
+		score := hash(vBytes, keyBytes)
+		log.Logger.Debugf("  track %s (version %s): %d", t, v, score)
+		if score > maxScore || (score == maxScore && bytes.Compare(vBytes, maxNode) < 0) {
+			maxScore = score
+			maxNode = vBytes
+		}
+	}
+	return string(maxNode)
+}
+
+var hasher = crc32.New(crc32.MakeTable(crc32.Castagnoli))
+
+func hash(node, key []byte) uint32 {
+	hasher.Reset()
+	hasher.Write(key)
+	hasher.Write(node)
+	return hasher.Sum32()
+}
 
 func Lookup(name string, user string) (*Version, error) {
 	// if user is not provided, use a random string
@@ -22,15 +50,13 @@ func Lookup(name string, user string) (*Version, error) {
 		return nil, errors.New("no versions found for application")
 	}
 
-	// use consistent to get version for user, fail if not present
-	v, err := app.c.Get(user)
-	if err != nil {
-		return nil, err
-	}
+	// use rendezvous hash to get version for user, fail if not present
+	v := rendezvousGet(name, user)
 	version, ok := app.versions[v]
 	if !ok {
 		return nil, errors.New("can't find version")
 	}
 
+	log.Logger.Debugf("lookup.Lookup :: version = %s, track = %s, ready = %t", version.Name, version.Track, version.Ready)
 	return &version, nil
 }
