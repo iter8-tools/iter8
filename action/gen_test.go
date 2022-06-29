7486e0c138b263702a7f019ce31c29680e1ef192
Srinivasan Parthasarathy <spartha@us.ibm.com>
2022-06-29 09:53:43 -0400

    Custom metrics fixes (#1304)
    * fixing curl
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * indent
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * fixing templates
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * fixing custom metrics tests
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * cleaning up provider URLs
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * fixing test ce
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * fixing custom metrics tests
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * test request body
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * noDownload flag is required
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- action/gen_test.go
+++ action/gen_test.go
@@ -67,7 +67,7 @@ func TestGenDB(t *testing.T) {
 	gOpts := NewGenOpts()
 	gOpts.ChartsParentDir = base.CompletePath("../", "")
 	gOpts.ChartName = "iter8"
-	gOpts.Values = []string{"tasks={custommetrics,assess}", "custommetrics.providerURLs[0]=https://raw.githubusercontent.com/iter8-tools/iter8/master/charts/iter8lib/templates/_metrics-istio.tpl", "custommetrics.common.providerURL=http://prometheus.istio-system:9090/api/v1/query", "custommetrics.values.destinationWorkload=httpbin-v2", "custommetrics.values.destinationWorkloadNamespace=default", `custommetrics.values.startingTime="2020-02-01T09:44:40Z"`, "assess.SLOs.upper.istio/error-rate=0"}
+	gOpts.Values = []string{"tasks={custommetrics,assess}", "custommetrics.templates.istio-prom=https://raw.githubusercontent.com/iter8-tools/iter8/master/charts/iter8lib/templates/_metrics-istio.tpl", "custommetrics.values.URL=http://prometheus.istio-system:9090/api/v1/query", "custommetrics.values.destinationWorkload=httpbin-v2", "custommetrics.values.destinationWorkloadNamespace=default", `custommetrics.values.startingTime="2020-02-01T09:44:40Z"`, "assess.SLOs.upper.istio/error-rate=0"}
 
 	err := gOpts.LocalRun()
 	assert.NoError(t, err)
@@ -84,7 +84,7 @@ func TestGenDB(t *testing.T) {
 	m := make(map[string]interface{}, 0)
 	b, _ := json.Marshal(exp.Spec[0])
 	json.Unmarshal(b, &m)
-	m = m["with"].(map[string]interface{})
-	s := m["providerURLs"].([]interface{})
-	assert.Equal(t, []interface{}{"https://raw.githubusercontent.com/iter8-tools/iter8/master/charts/iter8lib/templates/_metrics-istio.tpl"}, s)
+	w := m["with"].(map[string]interface{})
+	s := w["templates"].(map[string]interface{})
+	assert.Equal(t, map[string]interface{}{"istio-prom": "https://raw.githubusercontent.com/iter8-tools/iter8/master/charts/iter8lib/templates/_metrics-istio.tpl"}, s)
 }
