51b2584103a4f86e4171ca0a09b15a086d29424c
Srinivasan Parthasarathy <spartha@us.ibm.com>
2022-07-12 15:57:14 -0400

    Cmd docs (#1307)
    * fixing curl
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * indent
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    
    * align cmd and iter8.tools docs
    
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- doc.go
+++ doc.go
@@ -1,4 +1,3 @@
 // Package main is the entry point for the Iter8 CLI.
-// Iter8: Kubernetes testing platform built for DevOps, MLOps, SRE, and data science teams.
-// Iter8 experiments make it simple to collect performance and business metrics for apps and ML models, assess, compare and validate one or more app/ML model versions, promote the winning version, and maximize business value in each release.
+// Iter8 is the Kubernetes release optimizer built for DevOps, MLOps, SRE and data science teams. Iter8 makes it easy to ensure that Kubernetes apps and ML models perform well and maximize business value.
 package main
