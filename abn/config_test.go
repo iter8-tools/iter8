69acf4e13a71fd6bc51c3f8ec2338b97502dadbf
Michael Kalantar <kalantar@us.ibm.com>
2022-09-06 10:07:53 -0400

    tests were commented out
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- abn/config_test.go
+++ abn/config_test.go
@@ -16,11 +16,11 @@ func TestReadConfig(t *testing.T) {
 		numNamespaces int
 		numResources  int
 	}{
-		// {"empty", "config.empty.yaml", 0, 0},
+		{"empty", "config.empty.yaml", 0, 0},
 		{"nonamespaces", "config.nonamespaces.yaml", 0, 1},
-		// {"invalid", "config.invalid.yaml", 0, 0},
-		// {"garbage", "config.garbage.yaml", 0, 0},
-		// {"nofile", "config.nofile.yaml", 0, 0},
+		{"invalid", "config.invalid.yaml", 0, 0},
+		{"garbage", "config.garbage.yaml", 0, 0},
+		{"nofile", "config.nofile.yaml", 0, 0},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
 			c := readConfig(filepath.Join(dir, tt.file))
