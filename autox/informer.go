ada1609a1276ef6799597b513687864b8d4a9f2d
Srinivasan Parthasarathy <spartha@us.ibm.com>
2022-09-22 17:30:13 -0400

    autox helm actions
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

--- autox/informer.go
+++ autox/informer.go
@@ -3,7 +3,9 @@ package autox
 // informer.go - informer(s) to watch desired resources/namespaces
 
 import (
+	"errors"
 	"fmt"
+	"hash/maphash"
 	"reflect"
 
 	"github.com/iter8-tools/iter8/base/log"
@@ -13,7 +15,7 @@ import (
 )
 
 const (
-	autoxLabel = "iter8.tools/autox-group"
+	autoXLabel = "iter8.tools/autox-group"
 )
 
 type chartAction int64
@@ -23,15 +25,40 @@ const (
 	deleteAction  chartAction = 1
 )
 
-func getReleaseName(chartGroupName string, chartName string) string {
-	return fmt.Sprintf("autox-%s-%s", chartGroupName, chartName)
+// the name of a release will depend on:
+//
+//	the name of the releaseSpec,
+//	the id of the chart within the releaseSpec, and
+//	the set of (pruned) labels that triggers this release
+func getReleaseName(chartGroupName string, chartID string, prunedLabels map[string]string) string {
+
+	// use labels relevant to autoX to create a random hash value
+	// this value will be appended as a suffix in the release name
+	var hasher maphash.Hash
+	// chartGroupName and chartID are always hashed
+	_, _ = hasher.WriteString(chartGroupName)
+	_, _ = hasher.WriteString(chartID)
+
+	// adding version label
+	version := prunedLabels["app.kubernetes.io/version"]
+	_, _ = hasher.WriteString(version)
+
+	// adding track label
+	track := prunedLabels["iter8.tools/track"]
+	_, _ = hasher.WriteString(track)
+
+	nonce := fmt.Sprintf("%05x", hasher.Sum64())
+	nonce = nonce[:5]
+	return fmt.Sprintf("autox-%s-%s-%s", chartGroupName, chartID, nonce)
 }
 
-func installHelmReleases(resourceName string) {
-	doChartAction(resourceName, releaseAction)
+// installHelmReleases for a given chart group
+func installHelmReleases(prunedLabels map[string]string, namespace string) error {
+	return doChartAction(prunedLabels, releaseAction, namespace)
 }
 
-func installHelmRelease(releaseName string, chart chart) {
+// installHelmRelease for a given chart within a chart group
+func installHelmRelease(releaseName string, chart chart, namespace string) error {
 	// TODO: check if there is a preexisting Helm release
 
 	// TODO: mutex
@@ -39,13 +66,16 @@ func installHelmRelease(releaseName string, chart chart) {
 	// TODO: release Helm chart
 
 	log.Logger.Debug("Release chart:", releaseName)
+	return nil
 }
 
-func deleteHelmReleases(resourceName string) {
-	doChartAction(resourceName, deleteAction)
+// deleteHelmReleases for a given chart group
+func deleteHelmReleases(prunedLabels map[string]string, namespace string) error {
+	return doChartAction(prunedLabels, deleteAction, namespace)
 }
 
-func deleteHelmRelease(releaseName string, chart chart) {
+// deleteHelmRelease with a given release name
+func deleteHelmRelease(releaseName string, namespace string) error {
 	// TODO: check if there is a preexisting Helm release
 
 	// TODO: mutex
@@ -53,46 +83,78 @@ func deleteHelmRelease(releaseName string, chart chart) {
 	// TODO: delete Helm chart
 
 	log.Logger.Debug("Delete chart:", releaseName)
+	return nil
 }
 
-func doChartAction(resourceName string, chartAction chartAction) {
-	if cg, ok := iter8ChartGroupConfig[resourceName]; ok {
-		for chartName, chart := range cg.Charts {
-			releaseName := getReleaseName(resourceName, chartName)
-
+// doChartAction iterates through a given chart group, and performs action for each chart
+// action can be install or delete
+func doChartAction(prunedLabels map[string]string, chartAction chartAction, namespace string) error {
+	// get chart group name
+	chartGroupName := prunedLabels[autoXLabel]
+	// iterate through the charts in this chart group
+	var err error
+	if cg, ok := iter8ChartGroupConfig[chartGroupName]; ok {
+		for chartID, chart := range cg.Charts {
+			// get release name
+			releaseName := getReleaseName(chartGroupName, chartID, prunedLabels)
+			// perform action for this release
 			switch chartAction {
 			case releaseAction:
-				installHelmRelease(releaseName, chart)
-
+				// if there is an error, keep going forward in the for loop
+				if err1 := installHelmRelease(releaseName, chart, namespace); err1 != nil {
+					err = errors.New("one or more Helm release installs failed")
+				}
 			case deleteAction:
-				deleteHelmRelease(releaseName, chart)
+				// if there is an error, keep going forward in the for loop
+				if err1 := deleteHelmRelease(releaseName, namespace); err1 != nil {
+					err = errors.New("one or more Helm release deletions failed")
+				}
 			}
 		}
 	} else {
-		// TODO: what log level should this be?
-		log.Logger.Debug("AutoX should make a Helm release for app \"", resourceName, "\" but no Helm charts were provided in the chartGroupConfig")
+		log.Logger.Warnf("no matching chart group name in autoX group configuration: %s", chartGroupName)
 	}
+
+	if err != nil {
+		log.Logger.Error(err)
+	}
+
+	return err
 }
 
+// addObject is the function object that will be used as the add handler in the informer
 var addObject = func(obj interface{}) {
 	log.Logger.Debug("Add:", obj)
 
 	uObj := obj.(*unstructured.Unstructured)
-	resourceName := uObj.GetName()
-	// example label: iter8.tools/autox-group=hello
-	labels := pruneLabels(uObj.GetLabels())
 
-	// check if the app name matches the name in the autox label
-	if autoxLabelName, ok := labels[autoxLabel]; ok && resourceName == autoxLabelName {
-		// Release Helm charts
-		installHelmReleases(resourceName)
+	// if there is no autoX label, there is nothing to do
+	labels := uObj.GetLabels()
+	if !gotAutoXLabel(labels) {
+		return
 	}
+	// there is an autoX group name
+
+	// we will install Helm releases
+	prunedLabels := pruneLabels(labels)
+	installHelmReleases(prunedLabels, uObj.GetNamespace())
 }
 
+// pruneLabels will extract the labels that are relevant for autoX
 func pruneLabels(labels map[string]string) map[string]string {
-	// TODO: select labels important for autoX
+	prunedLabels := map[string]string{}
+	// if the autoX label exists, get it and stuff it into pruneLabels
+	if autoXLabelValue, ok := labels[autoXLabel]; ok {
+		prunedLabels[autoXLabel] = autoXLabelValue
+	}
+	// other labels like track and version can be potentially extracted in the future
+	return prunedLabels
+}
 
-	return labels
+// gotAutoXLabel checks if autoX label is present
+func gotAutoXLabel(labels map[string]string) bool {
+	_, ok := labels[autoXLabel]
+	return ok
 }
 
 var updateObject = func(oldObj, obj interface{}) {
