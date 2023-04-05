a823c61ddac53d1eab0a17bb814ceb07917c57c2
Srinivasan Parthasarathy <spartha@us.ibm.com>
2023-04-05 09:09:24 -0400

    config test
    Signed-off-by: Srinivasan Parthasarathy <spartha@us.ibm.com>

--- controllers/config_test.go
+++ controllers/config_test.go
@@ -1,1 +1,30 @@
 package controllers
+
+import (
+	"os"
+	"testing"
+
+	"github.com/iter8-tools/iter8/base"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+func TestReadConfig(t *testing.T) {
+	os.Setenv(configEnv, base.CompletePath("../", "testdata/controllers/config.yaml"))
+	c, e := readConfig()
+	assert.NoError(t, e)
+	assert.Equal(t, "30s", c.DefaultResync)
+	assert.Equal(t, 4, len(c.ResourceTypes))
+	isvc := c.ResourceTypes["isvc"]
+	assert.Equal(t, isvc, GroupVersionResourceConditions{
+		GroupVersionResource: schema.GroupVersionResource{
+			Group:    "serving.kserve.io",
+			Version:  "v1beta1",
+			Resource: "inferenceservices",
+		},
+		Conditions: []Condition{{
+			Name:   "Ready",
+			Status: "True",
+		}},
+	})
+}
