87a4ba9081c77e5464bf94f63ff77e6691e6b421
Alan Cha <Alan.cha1@ibm.com>
2023-05-10 09:53:28 -0400

    Revert back to iter8-tools.github.io
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

--- action/launch.go
+++ action/launch.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// DefaultHelmRepository is the URL of the default Helm repository
-	DefaultHelmRepository = "https://alan-cha.github.io/iter8"
+	DefaultHelmRepository = "https://iter8-tools.github.io/iter8"
 	// DefaultChartName is the default name of the Iter8 chart
 	DefaultChartName = "iter8"
 )
