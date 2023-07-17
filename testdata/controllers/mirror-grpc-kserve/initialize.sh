fa4b008d7efab7a4695e73588fb24bf474df995b
Michael Kalantar <kalantar@us.ibm.com>
2023-07-17 11:02:04 -0400

    update to v0.15 (#1543)
    * update to v0.15
    
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>
    
    * fix version reference
    
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>
    
    ---------
    
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- testdata/controllers/mirror-grpc-kserve/initialize.sh
+++ testdata/controllers/mirror-grpc-kserve/initialize.sh
@@ -43,7 +43,7 @@ metadata:
   labels:
     app.kubernetes.io/managed-by: iter8
     iter8.tools/kind: routemap
-    iter8.tools/version: v0.14
+    iter8.tools/version: v0.15
 data:
   strSpec: |
     versions: 
