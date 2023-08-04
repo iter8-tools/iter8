a13f95033b7ca8841a3ae62b1090bf1307433d4e
Alan Cha <Alan.cha1@ibm.com>
2023-08-04 09:48:13 -0400

    Add single endpoint multiple call test for gRPC
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

--- base/collect_grpc_test.go
+++ base/collect_grpc_test.go
@@ -18,6 +18,7 @@ import (
 
 const (
 	unary         = "unary"
+	unary2        = "unary2"
 	server        = "server"
 	client        = "client"
 	bidirectional = "bidirectional"
@@ -140,7 +141,7 @@ func TestRunCollectGRPCUnaryNoEndpoint(t *testing.T) {
 
 // Credit: Several of the tests in this file are based on
 // https://github.com/bojand/ghz/blob/master/runner/run_test.go
-func TestRunCollectGRPCEndpoints(t *testing.T) {
+func TestRunCollectGRPCMultipleEndpoints(t *testing.T) {
 	// define METRICS_SERVER_URL
 	metricsServerURL := "http://iter8.default:8080"
 	err := os.Setenv(MetricsServerURL, metricsServerURL)
@@ -330,4 +331,97 @@ func TestRunCollectGRPCMultipleNoEndpoints(t *testing.T) {
 	err = ct.run(exp)
 	assert.NoError(t, err)
 	assert.True(t, metricsServerCalled)
+}
+
+func TestRunCollectGRPCSingleEndpointMultipleCalls(t *testing.T) {
+	// define METRICS_SERVER_URL
+	metricsServerURL := "http://iter8.default:8080"
+	err := os.Setenv(MetricsServerURL, metricsServerURL)
+	assert.NoError(t, err)
+
+	// mock metrics server
+	StartHTTPMock(t)
+	metricsServerCalled := false
+	MockMetricsServer(MockMetricsServerInput{
+		MetricsServerURL: metricsServerURL,
+		PerformanceResultCallback: func(req *http.Request) {
+			metricsServerCalled = true
+
+			// check query parameters
+			assert.Equal(t, myName, req.URL.Query().Get("experiment"))
+			assert.Equal(t, myNamespace, req.URL.Query().Get("namespace"))
+
+			// check payload
+			body, err := io.ReadAll(req.Body)
+			assert.NoError(t, err)
+			assert.NotNil(t, body)
+
+			// check payload content
+			bodyFortioResult := HTTPResult{}
+			err = json.Unmarshal(body, &bodyFortioResult)
+			assert.NoError(t, err)
+			assert.NotNil(t, body)
+
+			fmt.Println(string(body))
+
+			if _, ok := bodyFortioResult.EndpointResults[unary]; !ok {
+				assert.Fail(t, fmt.Sprintf("payload FortioResult.EndpointResult does not contain endpoint: %s", unary))
+			}
+
+			if _, ok := bodyFortioResult.EndpointResults[unary2]; !ok {
+				assert.Fail(t, fmt.Sprintf("payload FortioResult.EndpointResult does not contain endpoint: %s", unary2))
+			}
+		},
+	})
+
+	_ = os.Chdir(t.TempDir())
+	callType := helloworld.Unary
+	gs, s, err := internal.StartServer(false)
+	if err != nil {
+		assert.FailNow(t, err.Error())
+	}
+	t.Cleanup(s.Stop)
+
+	// valid collect GRPC task... should succeed
+	ct := &collectGRPCTask{
+		TaskMeta: TaskMeta{
+			Task: StringPointer(CollectGRPCTaskName),
+		},
+		With: collectGRPCInputs{
+			Config: runner.Config{
+				Host: internal.LocalHostPort,
+				Call: "helloworld.Greeter.SayHello",
+			},
+			Endpoints: map[string]runner.Config{
+				unary: {
+					Data: map[string]interface{}{"name": "bob"},
+				},
+				unary2: {
+					Data: map[string]interface{}{"name": "charles"},
+				},
+			},
+		},
+	}
+
+	log.Logger.Debug("dial timeout before defaulting... ", ct.With.DialTimeout.String())
+
+	exp := &Experiment{
+		Spec:   []Task{ct},
+		Result: &ExperimentResult{},
+		Metadata: ExperimentMetadata{
+			Name:      myName,
+			Namespace: myNamespace,
+		},
+	}
+	exp.initResults(1)
+	err = ct.run(exp)
+
+	log.Logger.Debug("dial timeout after defaulting... ", ct.With.DialTimeout.String())
+
+	assert.NoError(t, err)
+	assert.Equal(t, exp.Result.Insights.NumVersions, 1)
+	assert.True(t, metricsServerCalled)
+
+	count := gs.GetCount(callType)
+	assert.Equal(t, 400, count)
 }
