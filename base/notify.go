47ab31b9abe6c80548024aee30a3e6a70cb4634d
Alan Cha <Alan.cha1@ibm.com>
2023-08-29 09:41:01 -0400

    Add name and namespace
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

--- base/notify.go
+++ base/notify.go
@@ -76,6 +76,12 @@ func getReport(exp *Experiment) map[string]Report {
 			NoTaskFailures:    exp.NoFailure(),
 			NumTasks:          len(exp.Spec),
 			NumCompletedTasks: exp.Result.NumCompletedTasks,
+			Experiment: &Experiment{
+				Metadata: ExperimentMetadata{
+					Name:      exp.Metadata.Name,
+					Namespace: exp.Metadata.Namespace,
+				},
+			},
 		},
 	}
 }
